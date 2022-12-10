cb09686ea011f80a1409281a439d4293b5a2388a
Farouq Ayofe <farouq.ayofe@visionr.com>
2022-12-10 21:57:28 +0000

    remove .next dir

--- .next/server/font-loader-manifest.js
+++
@@ -1,6 +0,0 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {},
-  "app": {
-    "/Users/farouqayofe/Private/farouq-dev/node_modules/.pnpm/@next+font@13.0.6/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Manrope\",\"arguments\":[{\"weight\":[\"500\",\"600\",\"700\",\"800\"]}],\"variableName\":\"manropeFont\"}": []
-  }
-}
